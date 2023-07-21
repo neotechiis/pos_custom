042ef9ed368ac01045e7bfdb28c70315aedd753b
KishanPanchal25 <k.d.panchalofc@gmail.com>
2023-07-21 06:31:09 +0530

    Customizations pos_print / Printer Settings

---
+++ pos_custom/pos_custom/doctype/printer_settings/printer_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Kishan Panchal and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Printer Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pos_custom/pos_custom/doctype/printer_settings/printer_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Kishan Panchal and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class PrinterSettings(Document):
+	pass
