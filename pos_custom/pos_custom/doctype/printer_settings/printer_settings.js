3bc8a51b3aa2bfe2d0338e9dfe7c4aad73aeaac0
KishanPanchal25 <k.d.panchalofc@gmail.com>
2023-07-21 06:48:31 +0530

    printer options

--- pos_custom/pos_custom/doctype/printer_settings/printer_settings.js
+++ pos_custom/pos_custom/doctype/printer_settings/printer_settings.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Printer Settings', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
+		// Fetch the list of available printers
+		frappe.ui.form
+			.qz_connect()
+			.then(function () {
+				return qz.printers.find();
+			})
+			.then((data) => {
+				// Get the default_printer field control
+				let defaultPrinterField = frm.fields_dict['default_printer'];
 
-	// }
+				// Clear existing options
+				defaultPrinterField.df.options = [];
+
+				// Add new options from the list of available printers
+				for (let printer of data) {
+					defaultPrinterField.df.options.push(printer);
+				}
+
+				// Refresh the field to update the options
+				defaultPrinterField.refresh();
+			})
+			.catch((err) => {
+				// Handle any errors that may occur while fetching printers
+				console.error('Error fetching printers:', err);
+			});
+	}
 });
+
